9dbb2999bae5adf8583cb7f4d47bc512bba00b22
KamSaf <bopaf42@gmail.com>
2024-03-25 23:54:37 +0100

    Created user authentication function

--- src/auth.py
+++ src/auth.py
@@ -1,13 +1,20 @@
 import sqlite3
-from utils.utils import create_table, generate_salt, hash_password
+from utils.utils import create_table, hash_password, check_hash
 
-DEFAULT_DATABASE_PATH = "../database.db"
 
 
 class Auth:
+	"""
 
-	@staticmethod
-	def authenticate(email: str, password_plain: str, password_confirm: str):
+	"""
+
+	DEFAULT_DATABASE_PATH = "database.db"
+
+	def __init__(self, database_path: str = DEFAULT_DATABASE_PATH):
+		self.database_path = database_path
+
+
+	def authenticate(self, email: str, password_plain: str):
 		"""
 			Function for authenticating user credentials
 
@@ -15,20 +22,16 @@ class Auth:
 			--------------------------------------------
 			email: str => user email
 			password_plain :str => user password
-			password_confirm: str => user password confirmation
 		"""
-		assert(password_confirm == password_plain)
-		with sqlite3.connect(DEFAULT_DATABASE_PATH) as conn:
-			create_table(conn=conn)
-			salt = generate_salt()
+		conn = sqlite3.connect(self.database_path)
+		create_table(conn=conn)
+		cursor = conn.cursor()
+		user_data = cursor.execute(f"SELECT password_hash, salt FROM user WHERE email='{email}'").fetchone()
+		conn.close()
+		return check_hash(password_hash=user_data[0], password_plain=password_plain, salt=user_data[1])
 
-			cursor = conn.cursor()
-			cursor.execute("INSERT INTO user VALUES (:email, :password_hash, :salt)", {'email': email, 'password_hash': password_hash, 'salt': salt})
-			conn.commit()
-			conn.close()
 
-	@staticmethod
-	def save_user(email: str, password_plain: str, password_confirm: str) -> None:
+	def save_user(self, email: str, password_plain: str, password_confirm: str) -> None:
 		"""
 			Function saving user data to the database
 
@@ -40,24 +43,25 @@ class Auth:
 
 		"""
 		assert(password_confirm == password_plain)
-		with sqlite3.connect(DEFAULT_DATABASE_PATH) as conn:
-			create_table(conn=conn)
-			password_hash, salt = hash_password(password_plain=password_plain)
-			cursor = conn.cursor()
-			cursor.execute("INSERT INTO user VALUES (:email, :password_hash, :salt)", {'email': email, 'password_hash': password_hash, 'salt': salt})
-			conn.commit()
-			conn.close()
+		conn = sqlite3.connect(self.database_path)
+		create_table(conn=conn)
+		password_hash, salt = hash_password(password_plain=password_plain)
+		cursor = conn.cursor()
+		cursor.execute("INSERT INTO user VALUES (:email, :password_hash, :salt)", {'email': email, 'password_hash': password_hash, 'salt': salt})
+		conn.commit()
+		conn.close()
 
-	@staticmethod
-	def change_password():
+
+	def change_password(self):
 		pass
 
-	@staticmethod
-	def remind_password():
+
+	def remind_password(self):
 		pass
 
 
 
 if __name__ ==  "__main__":
-	save_user(email='email@domain.com', password_plain='password', password_confirm='password')
+	auth = Auth()
+	print(auth.authenticate(email='email@domain.com', password_plain='password'))
 	pass
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -12,8 +12,7 @@ def create_table(conn: Connection) -> None:
 			conn: Connection -> SQLite database connection
 	"""
 	cursor = conn.cursor()
-	cursor.execute("""CREATE TABLE IF NOT EXISTS user(email text, password_hash text, salt real) """)
-	print('created')
+	cursor.execute("""CREATE TABLE IF NOT EXISTS user(email text, password_hash text, salt text) """)
 	conn.commit()
 
 
@@ -28,7 +27,7 @@ def generate_salt() -> str:
 	ASCII_CHARACTERS_BOUNDS = [33, 128]
 
 	salt = []
-	for i in range(32):
+	for _ in range(32):
 		x = random.randint(	ASCII_CHARACTERS_BOUNDS[0], ASCII_CHARACTERS_BOUNDS[1])
 		salt.append(chr(x))
 	random.shuffle(salt)
@@ -37,17 +36,35 @@ def generate_salt() -> str:
 
 def hash_password(password_plain: str) -> tuple[str, str]:
 	"""
-		Function hashing password using SHA256 algorithm with randomly generated 32 characters long salt
+		Function hashing password using SHA256 algorithm with given or randomly generated 32 characters long salt
 
 		Parameters:
 		---------------------------------------
 		password_plain: str => user plain text password to be hashed
+		salt: str (optional) => salt which is used to hash password (for checking hash)
 	"""
 	salt = generate_salt()
 	h = hashlib.sha256()
 	h.update((password_plain + salt).encode())
 	password_hash = h.hexdigest()
 	return password_hash, salt
+
+
+def check_hash(password_hash: str, password_plain: str, salt: str) -> bool:
+	"""
+		Function checking whether given plain text passwords hash matches the one kept in database
+
+		Parameters:
+		--------------------------------------
+		password_hash: str => user password hash retrieved from the database
+		password_plain: str => plain text password to be verified
+		salt: str => user password salt retrieved from the database
+	"""
+	h = hashlib.sha256()
+	h.update((password_plain + salt).encode())
+	return password_hash == h.hexdigest()
+
+
 
 if __name__ ==  "__main__":
 	pass
